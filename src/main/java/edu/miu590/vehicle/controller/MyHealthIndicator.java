363e04aedc232e4a34b64bfdcc7c65b91e8269da
devbinod <binodpant.nep@gmail.com>
2022-10-23 20:11:54 -0500

    actuator added.

---
+++ src/main/java/edu/miu590/vehicle/controller/MyHealthIndicator.java
@@ -0,0 +1,18 @@
+package edu.miu590.vehicle.controller;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MyHealthIndicator implements HealthIndicator {
+
+    @Override
+    @GetMapping({"/","/health"})
+    public Health health() {
+        return Health.up().build();
+    }
+}
