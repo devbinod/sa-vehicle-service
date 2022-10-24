b0ac7b5e671c3e74c56c50628187f4f12cd1d0ff
devbinod <binodpant.nep@gmail.com>
2022-10-23 20:40:48 -0500

    swagger api added.

--- src/main/java/edu/miu590/vehicle/controller/MyHealthIndicator.java
+++ src/main/java/edu/miu590/vehicle/controller/MyHealthIndicator.java
@@ -3,7 +3,6 @@ package edu.miu590.vehicle.controller;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
