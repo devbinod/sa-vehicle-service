e353d86d019aeca84dd5007ec2e40c2d8fa75cc0
devbinod <binodpant.nep@gmail.com>
2022-10-24 10:27:56 -0500

    resilience4j added.

--- src/main/java/edu/miu590/vehicle/controller/VehicleController.java
+++ src/main/java/edu/miu590/vehicle/controller/VehicleController.java
@@ -6,15 +6,20 @@ import edu.miu590.vehicle.model.SearchVehicleDto;
 import edu.miu590.vehicle.model.VehicleDto;
 import edu.miu590.vehicle.model.VehicleRequestDto;
 import edu.miu590.vehicle.services.VehicleDao;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 public class VehicleController implements VehiclesApi {
 
     private final VehicleDao vehicleDao;
+    private static final Logger LOGGER = LoggerFactory.getLogger(VehicleController.class);
 
     public VehicleController(VehicleDao vehicleDao) {
         this.vehicleDao = vehicleDao;
@@ -29,7 +34,7 @@ public class VehicleController implements VehiclesApi {
 
     @Override
     public ResponseEntity<List<VehicleDto>> getAllVehicle() {
-      return ResponseEntity.ok(vehicleDao.findAll());
+        return ResponseEntity.ok(vehicleDao.findAll());
     }
 
     @Override
@@ -45,7 +50,7 @@ public class VehicleController implements VehiclesApi {
 
     @Override
     public ResponseEntity<VehicleDto> updateVehicleById(String id, VehicleRequestDto vehicleRequestDto) {
-        return ResponseEntity.ok(vehicleDao.updateById(id,vehicleRequestDto));
+        return ResponseEntity.ok(vehicleDao.updateById(id, vehicleRequestDto));
     }
 
 
@@ -56,7 +61,15 @@ public class VehicleController implements VehiclesApi {
 
 
     @Override
+    @Retry(name = "${spring.application.name}", fallbackMethod = "callBackData")
     public ResponseEntity<List<VehicleDto>> filterVehicle(SearchVehicleDto searchVehicleDto) {
+        LOGGER.info(searchVehicleDto.toString());
         return ResponseEntity.ok(vehicleDao.searchVehicleAvailability(searchVehicleDto));
+    }
+
+    public ResponseEntity<List<VehicleDto>> callBackData(SearchVehicleDto searchVehicleDto, Exception exception) {
+        LOGGER.info(exception.getMessage());
+
+        return ResponseEntity.ok(vehicleDao.findAll());
     }
 }
